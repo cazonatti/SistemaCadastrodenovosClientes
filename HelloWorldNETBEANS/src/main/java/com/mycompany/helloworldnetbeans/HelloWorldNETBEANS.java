de637a8fb85c2cadf3f37440251639b91db732a1
cazonatti <potterflow@outlook.com>
2026-04-16 16:19:52 -0300

    second commit aleatory

---
+++ HelloWorldNETBEANS/src/main/java/com/mycompany/helloworldnetbeans/HelloWorldNETBEANS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworldnetbeans;
+
+/**
+ *
+ * @author 4104
+ */
+public class HelloWorldNETBEANS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
